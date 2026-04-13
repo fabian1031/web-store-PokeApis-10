59c7922befec54f5731ec468cac263b6eaff0437
fabian1031 <lugia759@gmail.com>
2026-04-13 09:59:48 -0500

    js inicial

---
+++ script.js
@@ -0,0 +1,6 @@
+let input = document.getElementById("inputPokemon");
+let botonBuscar = document.getElementById("buscarValor");
+let informacion = document.getElementById("valorInformacion");
+let botonGuardar = document.getElementById("valorGuardar");
+let favoristos = document.getElementById("favoritosCargue");
+

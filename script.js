82c89bc0ac4a36ab119744bb462cd92f07624a7b
Luis Rodriguez <luedropa@gmail.com>
2026-04-13 10:36:43 -0500

    mi cambios

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ let informacion = document.getElementById("valorInformacion");
 let botonGuardar = document.getElementById("valorGuardar");
 let favoristos = document.getElementById("favoritosCargue");
 
+
+// async function obtenerUsuarios() {
+//   try {
+//     const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+
+//     if (!response.ok) {
+//       throw new Error("Error en la petición");
+//     }
+
+//     const pokemon = await response.json();
+//     console.log(pokemon);
+
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+obtenerUsuarios();

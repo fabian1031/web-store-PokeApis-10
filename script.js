82eb4b9b8a6b5b6b4140a9df8b5f2ebd4a0404d6
fabian1031 <lugia759@gmail.com>
2026-04-13 10:32:46 -0500

    funcion buscar

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ let informacion = document.getElementById("valorInformacion");
 let botonGuardar = document.getElementById("valorGuardar");
 let favoristos = document.getElementById("favoritosCargue");
 
-botonBuscar.addEventListener("click", () => {
+botonBuscar.addEventListener("click", function () {
+  searchPokemon();
+});
+
+function searchPokemon() {
   let traerPokemon = input.value.trim().toLocaleLowerCase();
   fetch(`https://pokeapi.co/api/v2/pokemon/${traerPokemon}`)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-     
       console.log(data);
     })
     .catch(function (error) {
       alert("¡Error! Pokémon no encontrado");
     });
-});
+}
+
+

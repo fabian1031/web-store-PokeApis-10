d4afd86c882d2a41be3c5b372cfda29ef6606044
fabian1031 <lugia759@gmail.com>
2026-04-13 10:23:08 -0500

    crea evento de fetch

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ let informacion = document.getElementById("valorInformacion");
 let botonGuardar = document.getElementById("valorGuardar");
 let favoristos = document.getElementById("favoritosCargue");
 
+botonBuscar.addEventListener("click", () => {
+  let traerPokemon = input.value.trim().toLocaleLowerCase();
+  fetch(`https://pokeapi.co/api/v2/pokemon/${traerPokemon}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+     
+      console.log(data);
+    })
+    .catch(function (error) {
+      alert("¡Error! Pokémon no encontrado");
+    });
+});
